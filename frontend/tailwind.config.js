25a6c9707f208c327bb9bb525efdd273556c8daf
Robert Westbury <haxified1@gmail.com>
2020-05-05 16:03:19 +0100

    feat: make purge options have a bigger range

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  purge: ['./src/**/*.tsx'],
 
   theme: {
     extend: {
