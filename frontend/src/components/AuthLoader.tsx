7bb23e9ed0146f802a6de7ce7e2154173f9f485c
Robert Westbury <haxified1@gmail.com>
2020-05-08 12:09:04 +0100

    feat: make auth loader use logo and add migrate script

--- frontend/src/components/AuthLoader.tsx
+++ frontend/src/components/AuthLoader.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState, useContext } from 'react';
 import { AuthContext } from '../store/AuthContext';
 import { API_URL } from '../util/api';
 
+import Logo from '../assets/logo.svg';
+
 export const AuthLoader: React.FC = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const authContext = useContext(AuthContext);
@@ -26,10 +28,7 @@ export const AuthLoader: React.FC = ({ children }) => {
   if (loading)
     return (
       <div className="mx-auto my-auto">
-        <div className="spinner">
-          <div className="double-bounce1"></div>
-          <div className="double-bounce2"></div>
-        </div>
+        <img src={Logo} alt="" className="fixed top-1/2 left-1/2" style={{ transform: 'translate(-50%, -50%)' }} />
       </div>
     );
 
